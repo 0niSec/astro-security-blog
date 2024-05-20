e7ac5719974f3c1b763ac8698d60ac17fc8171de
Nick Conklin <nconkl1@outlook.com>
2024-05-20 10:51:09 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  prefetch: {
+    prefetchAll: true,
+  },
+  trailingSlash: "ignore",
+  syntaxHighlight: "prism",
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "static",
+});
---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "0niSec";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const GITHUB_URL = "https://github.com/0niSec/astro-security-blog";
