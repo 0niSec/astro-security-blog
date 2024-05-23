827035209e74b60ce307564080876b30b3aba06b
Nick Conklin <nconkl1@outlook.com>
2024-05-22 21:07:13 -0400

    :heavy_plus_sign: added expressive code plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,43 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import remarkToc from "remark-toc";
 import tailwind from "@astrojs/tailwind";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://0nisec.blog",
   prefetch: {
-    defaultStrategy: "hover",
+    defaultStrategy: "hover"
   },
   trailingSlash: "ignore",
   syntaxHighlight: "prism",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), expressiveCode()],
   markdown: {
-    remarkPlugins: [[remarkToc, { heading: "table of contents" }]],
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: "append",
-          properties: { className: "anchor" },
-          content: {
-            type: "element",
-            tagName: "span",
-            properties: {
-              className: "icon icon-link",
-            },
-            children: [{ type: "text", value: "#" }],
-          },
+    remarkPlugins: [[remarkToc, {
+      heading: "table of contents"
+    }]],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
+      behavior: "append",
+      properties: {
+        className: "anchor"
+      },
+      content: {
+        type: "element",
+        tagName: "span",
+        properties: {
+          className: "icon icon-link"
         },
-      ],
-    ],
+        children: [{
+          type: "text",
+          value: "#"
+        }]
+      }
+    }]]
   },
-  output: "static",
+  output: "static"
 });
