539017efce885b37f40d586e4692537486238f51
Nick <nconkl1@outlook.com>
2024-05-24 18:41:47 -0400

    :sparkles: added "perfection" writeup

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,16 +6,20 @@ const OS = ["Windows", "Linux"] as const;
 export const collections = {
     writeup: defineCollection({
         type: "content",
-        schema: z.object({
-            title: z.string(),
-            topics: z.array(z.string()), // A tag listing of the types of topics
-            description: z.string(),
-            pubDate: z.coerce.date(),
-            updatedDate: z.coerce.date().optional(),
-            heroImage: z.string().optional(),
-            difficulty: z.enum(DIFFICULTY),
-            os: z.enum(OS),
-            isActive: z.boolean(),
-        }),
+        schema: ({ image }) =>
+            z.object({
+                title: z.string(),
+                topics: z.array(z.string()), // A tag listing of the types of topics
+                description: z.string(),
+                pubDate: z.coerce.date(),
+                updatedDate: z.coerce.date().optional(),
+                heroImage: image().refine((img) => img.width >= 720, {
+                    message: "Hero image must be at least 1080px wide",
+                }),
+                heroAlt: z.string(),
+                difficulty: z.enum(DIFFICULTY),
+                os: z.enum(OS),
+                isActive: z.boolean(),
+            }),
     }),
 };
