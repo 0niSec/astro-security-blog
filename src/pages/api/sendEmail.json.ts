4f6c5ef1e78106e6a8bf5bdc5fe3cb819ab35893
Nick <nconkl1@outlook.com>
2024-05-30 13:41:44 -0400

    :sparkles: added contact form

--- src/pages/api/sendEmail.json.ts
+++ src/pages/api/sendEmail.json.ts
@@ -1,6 +1,6 @@
-import type { APIRoute } from "astro";
+import type { APIContext, APIRoute } from "astro";
 import { Resend } from "resend";
-
+export const prerender = false;
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
 const ADMIN_EMAIL = import.meta.env.ADMIN_EMAIL;
 const DESTINATION_EMAIL = import.meta.env.DESTINATION_EMAIL;
@@ -19,9 +19,9 @@ export const POST: APIRoute = async ({ params, request }) => {
 
     const send = await resend.emails.send({
         from: ADMIN_EMAIL,
-        to: [DESTINATION_EMAIL],
+        to: DESTINATION_EMAIL,
         subject: `Contact Form: ${subject}`,
-        html: message,
+        html: `<b>It works!</b><p>${message}</p>`,
     });
 
     if (send.data) {
@@ -46,3 +46,73 @@ export const POST: APIRoute = async ({ params, request }) => {
         );
     }
 };
+
+// export async function POST(context: APIContext): Promise<Response> {
+//     // Parse form data
+//     const formData = context.request.formData();
+//     const name = (await formData).get("name");
+//     const email = (await formData).get("email");
+//     const subject = (await formData).get("subject");
+//     const message = (await formData).get("message");
+
+//     // Validate form data
+//     if (!name || !email || !subject || !message) {
+//         return new Response(null, {
+//             status: 404,
+//             statusText:
+//                 "Please check all items and make sure all are filled out prior to sending.",
+//         });
+//     }
+
+//     // Send the email
+//     const emailSent = await sendContactEmail(
+//         name as string,
+//         email as string,
+//         subject as string,
+//         message as string,
+//     );
+
+//     if (!emailSent) {
+//         return new Response("Failed to send email", { status: 500 });
+//     }
+
+//     return context.redirect("/contact");
+// }
+
+// export async function sendContactEmail(
+//     name: string,
+//     email: string,
+//     subject: string,
+//     message: string,
+// ): Promise<boolean> {
+//     try {
+//         const send = await resend.emails.send({
+//             from: ADMIN_EMAIL,
+//             to: DESTINATION_EMAIL,
+//             subject: `0nisec Blog - ${subject}`,
+//             html: `
+//             <style>
+//             p {
+//                 margin: 0
+//                 padding: 0
+//             }
+//             </style>
+//             <h2>0niSec Blog - ${subject}</h2>
+//             <p>From: ${name}</p>
+//             <p>Email: ${email}</p>
+//             <p>${message}</p>
+//             `,
+//             text: message,
+//         });
+
+//         if (!send.data) {
+//             console.error(send.error);
+//             return false;
+//         }
+
+//         return true;
+//     } catch (error) {
+//         console.error(error);
+//         return false;
+//     }
+// }
