75f31093550214ea612827ae36fa5409e5487224
Nick Conklin <nconkl1@outlook.com>
2024-06-03 22:53:37 -0400

    :rocket: added date to table

--- db/config.ts
+++ db/config.ts
@@ -1,21 +1,21 @@
-import { column, defineTable, isNotNull } from "astro:db";
+import { NOW, column, defineTable, isNotNull } from "astro:db";
 import { defineDb } from "astro:db";
 
 const User = defineTable({
-    columns: {
-        name: column.text(),
-    },
+  columns: {
+    name: column.text(),
+  },
 });
 
 const Comment = defineTable({
-    columns: {
-        user: column.text(),
-        comment: column.text(),
-        date: column.date(),
-    },
+  columns: {
+    user: column.text(),
+    comment: column.text(),
+    date: column.date({ default: NOW }),
+  },
 });
 
 // https://astro.build/db/config
 export default defineDb({
-    tables: { User, Comment },
+  tables: { User, Comment },
 });
--- src/pages/api/addComment.ts
+++ src/pages/api/addComment.ts
@@ -3,29 +3,31 @@ import { db, Comment } from "astro:db";
 export const prerender = false;
 
 export const POST: APIRoute = async ({ request }) => {
-    const data = await request.json();
-    const { user, comment } = data;
+  const data = await request.json();
+  const { user, comment, date } = data;
 
-    // If the user isn't passed in the form POST then return a 403
-    if (!user) {
-        return new Response(null, {
-            status: 403,
-            statusText:
-                "You do not have access to make this request. Please authenticate and try again.",
-        });
-    }
+  console.log("Date", date);
 
-    // This shouldn't really happen but if somehow the form is submitted without a comment then return a 400
-    if (!comment) {
-        return new Response(null, {
-            status: 400,
-            statusText: "No comment submitted",
-        });
-    } else {
-        await db.insert(Comment).values({ user, comment });
-        return new Response(null, {
-            status: 200,
-            statusText: "OK",
-        });
-    }
+  // If the user isn't passed in the form POST then return a 403
+  if (!user) {
+    return new Response(null, {
+      status: 403,
+      statusText:
+        "You do not have access to make this request. Please authenticate and try again.",
+    });
+  }
+
+  // This shouldn't really happen but if somehow the form is submitted without a comment then return a 400
+  if (!comment) {
+    return new Response(null, {
+      status: 400,
+      statusText: "No comment submitted",
+    });
+  } else {
+    await db.insert(Comment).values({ user, comment });
+    return new Response(null, {
+      status: 200,
+      statusText: "OK",
+    });
+  }
 };
