c31164aa84bbd40e3b0a46fbd597e07c3c47beb8
Nick Conklin <nconkl1@outlook.com>
2024-06-06 15:41:46 -0400

    :bug: fixed an issue with the comments endpoint prerendering

--- src/pages/api/comments.ts
+++ src/pages/api/comments.ts
@@ -1,11 +1,12 @@
+export const prerender = false;
 import type { APIRoute } from "astro";
 import { db, Comment } from "astro:db";
 
 export const GET: APIRoute = async () => {
-    const comments = await db.select().from(Comment);
+  const comments = await db.select().from(Comment);
 
-    return new Response(JSON.stringify(comments), {
-        status: 200,
-        statusText: "OK",
-    });
+  return new Response(JSON.stringify(comments), {
+    status: 200,
+    statusText: "OK",
+  });
 };
